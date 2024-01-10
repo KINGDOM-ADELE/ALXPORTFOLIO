8b726221b76166ae9e1f64ea91c7a4b24f04a081
IZZYLOVE <izzylovu@gmail.com>
2024-01-10 05:30:55 +0100

    buildtemplates

--- node_server/app.js
+++ node_server/app.js
@@ -65,13 +65,13 @@ app.use('/api/v1/contactmessages', contactMessageRouter)// mounting contact mess
 
 
     
-    app.use(express.static(path.join(__dirname, 'build'))); // Serve static files from the "public" directory (React build files).
+    app.use(express.static(path.join(__dirname, 'buildtemplates'))); // Serve static files from the "public" directory (React build files).
     app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // lets us access static files in the upload folder
   
 
     // Define a route to serve the "build/index.html" file as the default route
     app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'buildtemplates', 'index.html'));
     });
 
 //   //DEFAULT ROUTE (for all other routes that are not matched)

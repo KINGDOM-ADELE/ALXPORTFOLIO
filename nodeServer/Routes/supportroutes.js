75e19c38b82124e4b2c9c8b27c13a8805b6b956a
IZZYLOVE <izzylovu@gmail.com>
2024-04-21 16:55:30 +0100

    fixed the bug

--- nodeServer/Routes/supportroutes.js
+++ nodeServer/Routes/supportroutes.js
@@ -1,24 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
- const supportController = require('../Controllers/supportController')
+ const supportcvController = require('../Controllers/supportcvController')
  const authController = require('../Controllers/authcontroller')
  const upload = require('../Utils/filehandler')
 
- 
-// ROUTES CHAINING for SUPPORT
-
-router.route('/allon_ticket_id/:_id')
-    .get(authController.protect,supportController.getAllSupportsOn_ticket_id)
+// ROUTES CHAINING for supportcv
 
 router.route('/')
-    .get(authController.protect,supportController.getSupports)
-    .post(authController.protect,authController.filesToSupportsPath,upload.array('files'),supportController.postSupport) //allows multiple files uploads
+    .get(authController.protect,supportcvController.getSupportcvs)
+    .post(authController.protect,authController.filesTosupportcvsPath,upload.array('files'),supportcvController.postSupportcv) //allows multiple files uploads
 
 router.route('/:_id')
-    .get(authController.protect,supportController.getSupport)
-    .patch(authController.protect,supportController.patchSupport)
-    .put(authController.protect,supportController.putSupport)
-    .delete(authController.protect,authController.restrict('admin'),supportController.deleteSupport)// for single role
+    .get(authController.protect,supportcvController.getSupportcv)
+    .patch(authController.protect,supportcvController.patchSupportcv)
+    .put(authController.protect,supportcvController.putSupportcv)
+    .delete(authController.protect,authController.restrict('admin'),supportcvController.deleteSupportcv)// for single role
 
 module.exports = router
